23c42a6b7ea15be8aa44a277bcb6978ff6d36dcb
X-Slasher <148427695+X-S-L-A-S-H-E-R@users.noreply.github.com>
2024-06-01 11:42:51 +0600

    Update ytdl.js

--- iris_plugins/ytdl.js
+++ iris_plugins/ytdl.js
@@ -45,4 +45,101 @@ CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
 
 
 
-const _0x21596d=_0xe43b;function _0x5713(){const _0x1aa716=['title','downloader','Instagram\x20downloader','2963450PRxKOg','all','_Youtube\x20Downloader_\x0a\x0a','639UBbPcK','../lib/functions.js','sendMessage','yt-search','5rnjRyG','\x0aURL\x20:\x20','name','channel','../lib/commands.js','(((.+)+)+)+$','downloadUrl','adreply','862661ANIHST','key','buffer','data','toString','search','json','\x0aDURATION\x20:\x20','https://api-aswin-sparky.koyeb.app/api/search/xvideos?search=','920208YdhrtM','constructor','quoted','get','2010594BANcHt','thumbnail','1174626WjSgBS','_Downloading\x20:\x20','649944DNYVPr','axios','duration','To\x20download\x20song','node-fetch','node-id3','\x0aTITLE\x20:\x20','jid','xv\x20search\x20results\x0a\x0a','_Enter\x20a\x20Query\x20!_','_Searching..._','https://api-viper-x.koyeb.app/api/song?name=','reply','147912zaCLNS'];_0x5713=function(){return _0x1aa716;};return _0x5713();}(function(_0x1a124f,_0x310417){const _0x58592a=_0xe43b,_0x19488d=_0x1a124f();while(!![]){try{const _0x274f6b=parseInt(_0x58592a(0x114))/0x1+-parseInt(_0x58592a(0x121))/0x2+-parseInt(_0x58592a(0x125))/0x3+parseInt(_0x58592a(0x11d))/0x4+parseInt(_0x58592a(0x13d))/0x5*(-parseInt(_0x58592a(0x123))/0x6)+-parseInt(_0x58592a(0x136))/0x7+-parseInt(_0x58592a(0x132))/0x8*(-parseInt(_0x58592a(0x139))/0x9);if(_0x274f6b===_0x310417)break;else _0x19488d['push'](_0x19488d['shift']());}catch(_0x9f9ad1){_0x19488d['push'](_0x19488d['shift']());}}}(_0x5713,0x89c8e));const _0x4f10a0=(function(){let _0x415ea0=!![];return function(_0xb5c78c,_0x51becc){const _0x255825=_0x415ea0?function(){if(_0x51becc){const _0x17d9db=_0x51becc['apply'](_0xb5c78c,arguments);return _0x51becc=null,_0x17d9db;}}:function(){};return _0x415ea0=![],_0x255825;};}()),_0x7dfc2d=_0x4f10a0(this,function(){const _0x2ef719=_0xe43b;return _0x7dfc2d[_0x2ef719(0x118)]()[_0x2ef719(0x119)](_0x2ef719(0x142))[_0x2ef719(0x118)]()[_0x2ef719(0x11e)](_0x7dfc2d)[_0x2ef719(0x119)](_0x2ef719(0x142));});_0x7dfc2d();const {iris,commands,isPublic}=require(_0x21596d(0x141)),{existsSync,mkdirSync,writeFileSync,readFileSync,createWriteStream}=require('fs'),fetch=require(_0x21596d(0x129)),yts=require(_0x21596d(0x13c)),ytdl=require('youtubedl-core'),NodeID3=require(_0x21596d(0x12a)),sleep=_0x5bdf87=>new Promise(_0x51c928=>setTimeout(_0x51c928,_0x5bdf87)),axios=require(_0x21596d(0x126)),{AddMp3Meta}=require(_0x21596d(0x13a));function _0xe43b(_0x2a5f4a,_0x2b363f){const _0x4fbd5a=_0x5713();return _0xe43b=function(_0x7dfc2d,_0x4f10a0){_0x7dfc2d=_0x7dfc2d-0x114;let _0x571300=_0x4fbd5a[_0x7dfc2d];return _0x571300;},_0xe43b(_0x2a5f4a,_0x2b363f);}iris({'name':'xv','fromMe':isPublic,'desc':_0x21596d(0x135),'category':_0x21596d(0x134)},async({m:_0x175c4f,client:_0x1e5761,args:_0x16412c})=>{const _0x390c44=_0x21596d;let _0x43da56=await fetch(_0x390c44(0x11c)+_0x16412c);var _0x578917=await _0x43da56[_0x390c44(0x11a)]();let _0xa68dbe=_0x390c44(0x12d);for(let _0x347348=0x1;_0x347348<0xb;_0x347348++){_0xa68dbe+=_0x390c44(0x12b)+_0x578917[_0x390c44(0x117)][_0x347348][_0x390c44(0x133)]+_0x390c44(0x11b)+_0x578917[_0x390c44(0x117)][_0x347348][_0x390c44(0x127)]+_0x390c44(0x13e)+_0x578917['data'][_0x347348]['url']+'\x0a';}return _0x175c4f[_0x390c44(0x144)](''+_0xa68dbe);}),iris({'name':'yt','fromMe':isPublic,'category':_0x21596d(0x134),'desc':'To\x20download\x20yt\x20vid/aud'},async({m:_0x570db9,client:_0x299473,args:_0x200800})=>{const _0x15e2f0=_0x21596d;if(!_0x200800)return _0x570db9[_0x15e2f0(0x131)]('_Enter\x20a\x20Query\x20!_');let _0x520d1d=await _0x299473[_0x15e2f0(0x13b)](_0x570db9[_0x15e2f0(0x12c)],{'text':_0x15e2f0(0x12f)},{'quoted':_0x570db9}),_0x3eccca=_0x15e2f0(0x138),_0x18542f=await yts(''+_0x200800),_0x1c5384=_0x18542f[_0x15e2f0(0x137)];for(let _0x13be2b=0x1;_0x13be2b<0xb;_0x13be2b++){_0x3eccca+='_'+_0x13be2b+'\x20.'+_0x1c5384[_0x13be2b][_0x15e2f0(0x133)]+'_\x0a';}return _0x299473[_0x15e2f0(0x13b)](_0x570db9['jid'],{'text':''+_0x3eccca,'edit':_0x520d1d[_0x15e2f0(0x115)]});}),iris({'name':'play','fromMe':isPublic,'category':_0x21596d(0x134),'desc':_0x21596d(0x128)},async({m:_0x56ec1e,client:_0x321e2b,args:_0x30c0d5})=>{const _0xa1c2b2=_0x21596d;_0x30c0d5=_0x30c0d5||_0x56ec1e[_0xa1c2b2(0x11f)]?.['text'];if(!_0x30c0d5)return _0x56ec1e[_0xa1c2b2(0x131)](_0xa1c2b2(0x12e));let _0x4996be=await _0x321e2b['sendMessage'](_0x56ec1e[_0xa1c2b2(0x12c)],{'text':_0xa1c2b2(0x12f)},{'quoted':_0x56ec1e});const _0x4b8a44=await axios[_0xa1c2b2(0x120)](_0xa1c2b2(0x130)+_0x30c0d5);let _0x334936=await _0x4b8a44[_0xa1c2b2(0x117)],_0x16cbb4=await(await fetch(''+_0x334936['data'][_0xa1c2b2(0x122)]))['buffer']();_0x321e2b[_0xa1c2b2(0x13b)](_0x56ec1e[_0xa1c2b2(0x12c)],{'text':_0xa1c2b2(0x124)+_0x334936['data'][_0xa1c2b2(0x133)]+'_','edit':_0x4996be[_0xa1c2b2(0x115)]});const _0x111c8f=await(await fetch(''+_0x334936[_0xa1c2b2(0x117)][_0xa1c2b2(0x143)]))[_0xa1c2b2(0x116)](),_0x51fcb1=await AddMp3Meta(_0x111c8f,_0x16cbb4,{'title':_0x334936[_0xa1c2b2(0x117)]['title'],'artist':_0x334936[_0xa1c2b2(0x117)][_0xa1c2b2(0x140)][_0xa1c2b2(0x13f)]});return await _0x321e2b['sendMessage'](_0x56ec1e[_0xa1c2b2(0x12c)],{'audio':_0x51fcb1,'mimetype':'audio/mpeg'},{'quoted':_0x56ec1e});});
+
+
+const { 
+  iris, 
+  commands, 
+  isPublic 
+} = require("../lib/commands.js");
+const { 
+  existsSync, 
+  mkdirSync, 
+  writeFileSync,
+  readFileSync,
+  createWriteStream
+} = require('fs');
+const fetch = require('node-fetch')
+const yts = require("yt-search")
+const ytdl = require("youtubedl-core");
+const NodeID3 = require('node-id3')
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const axios = require("axios")
+const {
+  AddMp3Meta
+} = require("../lib/functions.js");
+
+
+iris(
+    {
+        name: "xv",
+        fromMe: isPublic,
+        desc: "Instagram downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+let sample = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/xvideos?search=${args}`);
+var data = await sample.json();
+let txt = `xv search results\n\n`;
+      
+        for (let i=1; i<11; i++){
+  txt+=`
+TITLE : ${data.data[i].title}
+DURATION : ${data.data[i].duration}
+URL : ${data.data[i].url}\n`
+        }
+return m.adreply(`${txt}`)
+    }
+  );
+
+iris(
+    {
+        name: "yt",
+        fromMe: isPublic,
+        category: "downloader",
+        desc: "To download yt vid/aud"
+    },
+async ({
+
+        m, client, args
+
+    }) => {
+
+    if (!args) return m.reply("_Enter a Query !_")
+   let mes = await client.sendMessage(m.jid, { text : `_Searching..._`}, {quoted : m })
+    let datai = `_Youtube Downloader_\n\n`
+    let search = await yts(`${args}`)
+    let hdata = search.all
+ 
+    for (let i=1; i<11; i++){
+        datai += `_${i} .${hdata[i].title}_\n`
+    }
+   return client.sendMessage(m.jid, { text : `${datai}` , edit : mes.key })
+      }
+    )
+
+        
+iris(
+    {
+        name: "play",
+        fromMe: isPublic,
+        category: "downloader",
+        desc: "To download song"
+    },
+    async ({
+        m, client, args
+    }) => {
+      args = args || m.quoted?.text;
+        if (!args) return m.reply("_Enter a Query !_")
+      let mes = await client.sendMessage(m.jid, { text : `_Searching..._` } , { quoted : m })
+   const res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${args}`)
+    let response = await res.data
+    let coverBuffer = await (await fetch(`${response.data.thumbnail}`)).buffer()
+     client.sendMessage(m.jid, { text : `_Downloading : ${response.data.title}_` , edit : mes.key })
+   const songbuff = await (await fetch(`${response.data.downloadUrl}`)).buffer()
+   const song = await AddMp3Meta(songbuff , coverBuffer , { title : response.data.title , artist : response.data.channel.name } )
+     return await client.sendMessage(m.jid , {audio : song ,  mimetype : 'audio/mpeg'} , { quoted : m })
+      
+    })
